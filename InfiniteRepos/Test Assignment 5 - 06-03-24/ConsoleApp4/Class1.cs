0e7256e1744731e26ea375532855e35f4311c8ed
mahsx <maheshsek@infinite.com>
2024-03-06 11:11:42 +0530

    6th march assignment

---
+++ InfiniteRepos/Test Assignment 5 - 06-03-24/ConsoleApp4/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // First Question
+            Undergraduate ug = new Undergraduate();  // creating Undergraduate object..
+            ug.StudentId = 1;
+            ug.Name = "Avinash Singh";
+            ug.grade = 56;
+            Console.WriteLine($"Undergraduate Passed-> { ug.Ispassed(ug.grade)}");
+
+            Graduate gg = new Graduate();         // creating Graduate object..
+
+            gg.StudentId = 2;
+            gg.Name = "Avi";
+            gg.grade = 89;
+            Console.WriteLine($"Graduate Passed->  {gg.Ispassed(gg.grade)}");
+        }
+    }
+}
---
+++ InfiniteRepos/Test Assignment 5 - 06-03-24/ConsoleApp4/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Student
+    {
+        public string Name;
+        public int StudentId;
+        public int grade;
+        abstract public bool Ispassed(int grade); // Abstract method
+
+    }
+    class Undergraduate : Student
+    {
+        public override bool Ispassed(int grade)
+        {
+            if (grade > 70)
+                return true;
+            else
+                return false;
+        }
+    }
+    class Graduate : Student
+    {
+        public override bool Ispassed(int grade)
+        {
+            if (grade > 80)
+                return true;
+            else
+                return false;
+
+        }
+
+    }
+}
